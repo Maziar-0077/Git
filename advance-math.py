442fbf7426c7df70dffd551a3983f1450dc82305
Maziar Ghareni <maziarghareni7799@yahoo.com>
2026-09-14 19:04:27 +0330

    Advanced-math Update

--- advance-math.py
+++ advance-math.py
@@ -1,5 +1,18 @@
+import random
+
 def f(x: float) -> float:
     return x ** 2
 
 def df(x: float) -> float:
     return 2 * x
+
+
+
+def Optimizer(max_iterations: int, lr: float) -> float:
+    x = random.randn()
+    for i in range(max_iterations):
+        dx = df(x)
+        x -= lr * dx
+    return x
+
+print(Optimizer(100, 0.1))
